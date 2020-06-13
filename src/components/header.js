0cec9c3351738964cb11b8c2107db1d0c4f67784
Jacob Pfeifer <jacob@pfeifer.dev>
2020-06-12 22:02:38 -0400

    add garden link

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,33 @@
 import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles(theme => ({
+  link: {
+    textDecoration: 'none',
+  },
+  title: {
+    flexGrow: 1,
+  },
+  buttonLink: {
+    color: 'white',
+  },
+}));
 const Header = props => {
-  const { classes } = props;
-  console.log(classes);
+  const classes = useStyles();
   return (
     <AppBar position="static" style={{ marginBottom: 20 }}>
       <Toolbar>
-        <Typography variant="h6">{props.siteTitle}</Typography>
+        <Typography variant="h6" className={classes.title}>
+          {props.siteTitle}
+        </Typography>
+        <Link className={classes.link} to="/garden">
+          <Button color="inherit" className={classes.buttonLink}>
+            Garden
+          </Button>
+        </Link>
       </Toolbar>
     </AppBar>
   );
